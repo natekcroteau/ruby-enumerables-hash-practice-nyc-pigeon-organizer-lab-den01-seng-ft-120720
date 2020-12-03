afb6dc69671480a1d4135bf08a086d8c1420885a
Nate Croteau <natekcroteau@gmail.com>
2020-12-03 21:22:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,17 +1,17 @@
-require 'pry'  #require pry 
+require 'pry'                                              #require pry 
  
 def nyc_pigeon_organizer(data) 
   
-  findata = data.each_with_object({}) do |(k, v), f_arr| #each_with_object transforms hash to hash k=color,v=color value,f_arr is empty
-    v.each do |color, names|                            #for each color hash
-      names.each do |name|                      
-        if !f_arr[name]                         
-          f_arr[name] = {}                      
+  fin_data = data.each_with_object({}) do |(k, v), f_arr|  #each_with_object transforms hash to hash k=color,v=color value,f_arr is empty
+    v.each do |color, names|                              #for each color hash use the k,v combo = color,names
+      names.each do |name|                               #for each name value do
+        if !f_arr[name]                                 #if f_arr doesnt have that name
+          f_arr[name] = {}                             #then add it as a new hash 
         end
-        if !f_arr[name][k]                      
-          !f_arr[name][k] = [] 
+        if !f_arr[name][k]                            #if f_arr doesnt have the name => color then 
+          !f_arr[name][k] = []                        #add an array to collect color descriptor (if it doesnt already)
         end
-        f_arr[name][k].push(color.to_s)   
+        f_arr[name][k].push(color.to_s)               #push the color value into that array as a string 
      end 
     end 
   end 
@@ -32,31 +32,3 @@ end
 
 
 
-
-
-
-
-
-
-#require 'pry'
-
-#def nyc_pigeon_organizer(data)
-#  findata = data.each_with_object({}) do |(k, v), f_arr|  #k=color w/ v being the color value hash, f_arr is the empty hash 
-                                                      #binding.pry used here first to define the above
- #   v.each do |color, names|                      #for every v itterate for the k,v combo within that larger hash
-                                         #binding.pry here to determine color=purple and names=the names, what we are looking for!!!
-#      names.each do |name|               #target the now inner-looped value of name  
- #       if !f_arr[name]                    #and if it is not in f_arr[the current itterated name]
-  #        f_arr[name] = {}                   #then create a hash for it
-                                                #binding.pry here verifies that the f_arr created a new hash for the first name itterated 
-   #     end
-    #    if !f_arr[name][k]                      #if the current color(k) doesnt exist then
-     #     f_arr[name][k] = []                   #create a new array for that in the form of an array 
-      #  end
-       # findata[name][k].push(color.to_s)       #and fill in the colors for each itteration with each name 
-    
-     #end 
-    #end 
-  #end 
-  
-#end 
